d23f647812d3b1509c0fd1d0b0de86fa19b90a6a
Archer <15754600185@163.com>
2015-10-27 11:50:17 -0300

    Initialize bookstore rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookstore::Application.config.secret_token = '6d6bf921ff78e299873fb6ab850799830a71b017e81eae404e25398696b8d3fc68b5605378457cb1d5da9ac05a7c053da4e9a44cfd978eb243bd44708c86a2b6'
